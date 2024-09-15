6b021ea70f24acfb1093a7391233af3c287c9175
JustClyde <hlonelamadubela@gmail.com>
2024-09-15 18:57:47 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,8 @@
 principal = 1000
 rate = 0.05
 time = 3
-interest = principal * rate * time
-P = 1000
-R = 0.05
-T = 3
-I = (P * R * T)
+[interest = principal * rate * time]
+
 print(interest)
-print(I)
+
 
